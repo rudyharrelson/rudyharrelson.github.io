af38d944f53865d4b055cea87d5a030ab3c3ad25
Rudolph Harrelson <rudyharrelson@gmail.com>
2020-03-15 04:11:37 -0400

    push

---
+++ precache-manifest.942de987643b78fdc6fa4e64d9005301.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99cf10258616528ec2977d26d7fe007f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a292cc064d0edad9f1e",
+    "url": "/static/css/main.c3982de0.chunk.css"
+  },
+  {
+    "revision": "b0c0151a8c24c2430880",
+    "url": "/static/js/2.c104b96d.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.c104b96d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a292cc064d0edad9f1e",
+    "url": "/static/js/main.f7e14a6a.chunk.js"
+  },
+  {
+    "revision": "366ddaea945e197457a1",
+    "url": "/static/js/runtime-main.ad264d5c.js"
+  }
+]);
